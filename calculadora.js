9c9536e59b9dd8cc9f0fb88d6034a99c1dc5e4eb
rpinsan348 <145375002+rpinsan348@users.noreply.github.com>
2025-02-03 11:18:24 +0100

    calculadora despliegue3

--- calculadora.js
+++ calculadora.js
@@ -8,5 +8,16 @@ function restar(a, b) {
   return a - b;
 }
 
+function multiplicar(a, b){
+  return a * b;
+}
 
-module.exports = { sumar, restar };
+function dividir(a, b){
+  return a/b;
+}
+
+function raizCuadrada(a){
+  return Math.sqrt(a)
+}
+
+module.exports = { sumar, restar ,multiplicar, dividir, raizCuadrada};
--- calculadora.test.js
+++ calculadora.test.js
@@ -1,4 +1,4 @@
-const { sumar, restar } = require('./calculadora');
+const { sumar, restar, multiplicar, dividir, raizCuadrada } = require('./calculadora');
 
 test('Suma 2 + 3 y devuelve 5', () => {
     expect(sumar(2, 3)).toBe(5);
@@ -6,4 +6,16 @@ test('Suma 2 + 3 y devuelve 5', () => {
 
 test('Resta 5 - 3 y devuelve 2', () => {
     expect(restar(5, 3)).toBe(2);
+});
+
+test('Multiplicar 5 * 3 y devuelve 15', () => {
+    expect(multiplicar(5, 3)).toBe(10);
+});
+
+test('Divide 6 / 2 y devuelve 3', () => {
+    expect(dividir(6, 2)).toBe(3);
+});
+
+test('Raiz cuadrada 16 y devuelve 4', () => {
+    expect(raizCuadrada(16)).toBe(4);
 });
