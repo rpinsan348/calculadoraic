507d3bab9aab53e305be61a93aba5b0aebe7ffc1
rpinsan348 <145375002+rpinsan348@users.noreply.github.com>
2025-02-10 11:12:44 +0100

    Calculadora

--- calculadora.js
+++ calculadora.js
@@ -27,7 +27,6 @@ function suma(a, b) { return a + b; } function resta(a, b) { return a - b; }
 function multiplicar(a, b) { return a * b; } function dividir(a, b) { if (b === 0) { throw new Error("No se puede dividir por cero"); } return a / b; } 
 // Hacer las funciones accesibles en el navegador 
 
-module.exports(suma, resta, multiplicar, dividir);
 /*window.suma = suma; 
 window.resta = resta; 
 window.multiplicar = multiplicar; 
