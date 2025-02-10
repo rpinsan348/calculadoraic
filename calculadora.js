78c13c7162a9d1b73dbfb622a4d81c6cd1ebc8a9
rpinsan348 <145375002+rpinsan348@users.noreply.github.com>
2025-02-10 11:12:05 +0100

    Calculadora

--- calculadora.js
+++ calculadora.js
@@ -26,7 +26,9 @@ module.exports = { sumar, restar, multiplicar, dividir, raizCuadrada };
 function suma(a, b) { return a + b; } function resta(a, b) { return a - b; } 
 function multiplicar(a, b) { return a * b; } function dividir(a, b) { if (b === 0) { throw new Error("No se puede dividir por cero"); } return a / b; } 
 // Hacer las funciones accesibles en el navegador 
-window.suma = suma; 
+
+module.exports(suma, resta, multiplicar, dividir);
+/*window.suma = suma; 
 window.resta = resta; 
 window.multiplicar = multiplicar; 
-window.dividir = dividir;
+window.dividir = dividir;*/
